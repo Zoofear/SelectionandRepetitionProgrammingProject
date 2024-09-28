616dc6c0c1e0c790caf24568b47e26f899d9a7a9
Zoofear <misterz0312@gmail.com>
2024-09-28 13:26:58 -0400

    Finished project one, used nested if statements to check witch operator is being used in the calculations

--- Selection and Repetition Programming Project/Selection and Repetition Programming Project.cpp
+++ Selection and Repetition Programming Project/Selection and Repetition Programming Project.cpp
@@ -10,7 +10,7 @@ int main()
 		//Variables
 	int int1;		//First integer input
 	int int2;		//Second integer input
-	char operate;	//The operator to use
+	char operate = '0';	//The operator to use
 	double answer;	//This is the output to the calculations
 		//Input
 	
@@ -22,10 +22,12 @@ int main()
 
 	cin >> int2;
 
-	cout << "Enter the operator (+ - / *) you want to use" << endl;	//Getting the operator to use in the calculations
+	while (operate != '/' && operate != '*' && operate != '+' && operate != '-') //While loop to keep asking for the correct operator, will repeat until an operator is used
+	{
+		cout << "Enter the operator (+ - / *) you want to use" << endl;	//Getting the operator to use in the calculations
 
-	cin >> operate;
-
+		cin >> operate;
+	}
 		//Divide by Zero check
 
 	if (operate == '/' && int2 == 0)
@@ -33,21 +35,40 @@ int main()
 	{
 		cout << "You cannot divide by 0" << endl;
 	}
-	else 
-	{
+	
 		//Calculations
+	
+	else if (operate == '/')
+	{	
+		answer =  static_cast <double> (int1) / int2;	//Division calculation, used static cast to change int1 to a floating point number to be able to divide into decimals without it being rounded to zero
 
-		answer = 
+		cout << "Your answer is " << answer << endl;
+	}
+	else if (operate == '*')
+	{
+		answer = int1 * int2;							//Multiplication calculation
 
-
+		cout << "Your answer is " << answer << endl;
+	}
+	else if (operate == '+')
+	{
+		answer = int1 + int2;							//Addition Calculation
+		cout << "Your answer is " << answer << endl;
+	}
+	else if (operate == '-')
+	{
+		answer = int1 - int2;							//Subtraction Calculation
+		
+		cout << "Your ansswer is " << answer << endl;
 	}
 	
+		//Error for somehow getting past the incorrect operator loop
 
-	
+	else
+	{
+		cout << "Erm something went wrong somehow" << endl;
+	}
 
-	
-
-
-
+	return 0;
 
 }
