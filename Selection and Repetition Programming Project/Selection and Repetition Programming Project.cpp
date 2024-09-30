eda92d393d35b5eb089a4312221319b0d9232bdc
Zoofear <misterz0312@gmail.com>
2024-09-30 18:41:22 -0400

    Finished file 3 yippie! added the loops to read the file and the if then statement to compare and write out the files to file 3, then added the cleanup loops, everything has been tested and works with the provided files

--- Selection and Repetition Programming Project/Selection and Repetition Programming Project.cpp
+++ Selection and Repetition Programming Project/Selection and Repetition Programming Project.cpp
@@ -8,56 +8,91 @@
 using namespace std;
 
 int main()
-{
+{	
 		//Variables
-	char letter1;
-	char letter2;
-
-	string name1;
-	string name2;
-	string end = "\n";
-	
-	
-		//Initializing Files
-	ifstream file1("file1.txt");	//File 1 to be merged
-	ifstream file2("file2.txt");	//File 2 to be merged
-	ofstream file3("file3.txt"); //File 3 to have everything merged in
-		
-		//Making sure files are open
-
-		if (!file1)
-		{
-			cout << "Error opening file." << endl;
-		}
-	
-		if (!file2)
-		{
-			cout << "Error opening file." << endl;
-		}
-
-		if (!file3)
-		{
-			cout << "Error opening file." << endl;
-		}
-
-
-		//File reading loops
-	while (!file1.eof() || !file2.eof())
-		{
-		
-			//Getting the first letter of the name in the file to use ASCII value to determine which name to put in
-		file1 >> letter1;
-		file2 >> letter2;
 			
-			if (letter1 < letter2)
-				{
+	string name1;		//Name from file 1
+	string name2;		//Name from file 2
+	string end = "\n";	//Endline string used to organize the output file
+
+
+		//Initializing Files
+
+	ifstream file1;	//File 1 to be merged
+	ifstream file2;	//File 2 to be merged
+	ofstream file3;	//File 3 to have everything merged in
+
+		//Was having issues initializing and opening files together so just separated them
+	
+	file1.open("file1.txt");
+	file2.open("file2.txt");
+	file3.open("file3.txt");
+
+		//Making sure all files are opened
+	
+	if (file1.fail() || file2.fail() || file3.fail())
+	{
+		cout << "File no worky" << endl;
+	}
+	else
+	{
+
+			//File reading loops
+
+		while (!file1.eof() && !file2.eof())
+		{
+
+				//Getting the name in the file, Getline is ussed because file >> ends on a space and doesnt retrieve the name and grade together
+
+			getline(file1, name1);
+			getline(file2, name2);
+
+				//Checking alphabetically which file comes first comparing the first letters ASCII value
 				
-				file1 >> name1;
+			if (name1 < name2)
+			{
+				name1 = name1 + end;	//Adding a "/n" string to the string name 1 so when it is written into file 3 it can go to the next line in the output file
 
-				name1 = name1 + end;
+				name2 = name2 + end;	//the second name needs to be processed aswell as it also needs to be written to the file before the loop reads the next line in the file
 
-				file3 << name1;
+				file3 << name1;			//Writing the file in alphabetical order, in this if statment name 1 is before name 2
 
-				}
+				file3 << name2;			//This needs to be written or else it will be lost, This code wouldnt work if the files werent sorted beforehand
+			}
+			else
+			{
+				name2 = name2 + end;	//Adding a "/n" string to the string name 2 so when it is written into file 3 it can go to the next line in the output file
+
+				name1 = name1 + end;	//the second name needs to be processed aswell as it also needs to be written to the file before the loop reads the next line in the file
+
+				file3 << name2;			//Writing the file in alphabetical order, in this if statment name 2 is before name 1
+
+				file3 << name1;			//This needs to be written or else it will be lost, This code wouldnt work if the files werent sorted beforehand
+			}
 		}
+		
+			//This loop makes sure there are no remaning files in file 1, and if there are it will write it to file 3, It would only stay sorted if the file 1 and 2 were sorted
+
+		while (!file1.eof())
+		{
+			getline(file1, name1);
+
+			name1 + end;
+
+			file3 << name1;
+		}
+		
+			//This loop makes sure there are no remaning files in file 2, and if there are it will write it to file 3, It would only stay sorted if the file 1 and 2 were sorted
+		
+		while (!file2.eof())
+		{
+			getline(file2, name2);
+
+			name2 = name2 + end;
+
+			file3 << name2;
+		}
+	}
+
+	return(0); 
 }
